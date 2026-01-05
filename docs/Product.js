eae24b42701714e84008c463e7bea151993d0c3e
Vitalii <122177855+wentyy1@users.noreply.github.com>
2026-01-05 12:33:11 +0200

    Add Product class to manage product details

---
+++ docs/Product.js
@@ -0,0 +1,9 @@
+export class Product {
+  constructor(id, name, type, brand, price) {
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.brand = brand;
+    this.price = price;
+  }
+}
